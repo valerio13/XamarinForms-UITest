294a65b645c70b75ff1c0acc424d0198bfa3e19f
Valerio <valerio.colantonio@gmail.com>
2021-12-21 15:35:05 +0100

    Added UITest project

---
+++ UITest/AppInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.UITest;
+
+namespace UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            // TODO: If the iOS or Android app being tested is included in the solution 
+            // then open the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            //
+            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+            // installed. To start the Test Cloud Agent the following code should be
+            // added to the FinishedLaunching method of the AppDelegate:
+            //
+            //    #if ENABLE_TEST_CLOUD
+            //    Xamarin.Calabash.Start();
+            //    #endif
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    // TODO: Update this path to point to your Android app and uncomment the
+                    // code if the app is not included in the solution.
+                    //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
+                    .InstalledApp("com.valeriocolantonio.xamarinuitestintegration")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                // TODO: Update this path to point to your iOS app and uncomment the
+                // code if the app is not included in the solution.
+                //.AppBundle("../../../XamarinUITestIntegration.iOS/bin/iPhoneSimulator/Debug/XamarinUITestIntegration.iOS.app")
+                .InstalledApp("com.valeriocolantonio.XamarinUITestIntegration")
+                .DeviceIdentifier("AAB6349F-52F2-4FC7-AD0D-68921D71E1AA")
+                .EnableLocalScreenshots()
+                .StartApp();
+        }
+    }
+}
---
+++ UITest/Tests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]        
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void NavigateToSecondPage()
+        {
+            /* Repl opens a conskole window to test UITest commands.
+             * Writing on console app. intellicense shows options.
+             * With "tree" command is possible to see all components in the page.
+            */
+            //app.Repl();
+
+            //Marked find elements by Id, Content-desc or Text
+            app.Tap(x => x.Marked("Next Page"));
+            app.Screenshot("Second page");
+            app.Back();
+        }
+
+        [Test]
+        public void ChangeSliderValue()
+        {            
+            app.SetSliderValue(x => x.Marked("slider"), 25.55);
+            app.Query(x => x.Marked("sliderText").Property("Text").Contains("25.55"));
+            app.Screenshot("Slider test");            
+        }
+    }
+}
