9ec2383d7b7d7ea88381cfa930d09af3fb627e28
Valerio <valerio.colantonio@gmail.com>
2021-12-21 15:34:03 +0100

    Added Xamarin.Forms project

---
+++ XamarinUITestIntegration/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinUITestIntegration
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ XamarinUITestIntegration/SecondPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinUITestIntegration
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
